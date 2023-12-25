bcacdebfb58f1156fa4bbb5d33baad829579ea5d
venkateshwebdev <nikhilsirigineedi3@gmail.com>
2023-12-25 06:41:44 +0530

    final

--- src/components/threejs/Scene.jsx
+++ src/components/threejs/Scene.jsx
@@ -8,7 +8,7 @@ const Scene = () => {
       <ambientLight />
       <CoolMan />
       <Html className="w-screen" center>
-        <div className="w-full mt-[4300px] flex flex-col gap-5 items-center justify-center text-white p-10">
+        <div className="w-full mt-[4300px] max-md:mt-[4700px] flex flex-col gap-5 items-center justify-center text-white p-10">
           <div className="text-5xl italic text-transparent font-bold uppercase text-center flex flex-col" style={{WebkitTextStroke:"1px white"}}>Venkatesh,<span>Web developer</span></div>
           <p className="text-lg w-1/2 max-md:w-full">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste a soluta quisquam neque dolores, iure sit veniam asperiores eum ipsam distinctio aliquid ducimus cupiditate facere voluptas nisi adipisci, mollitia recusandae.</p>
           <p className="text-lg w-1/2 max-md:w-full">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste a soluta quisquam neque dolores, iure sit veniam asperiores eum ipsam distinctio aliquid ducimus cupiditate facere voluptas nisi adipisci, mollitia recusandae.</p>
