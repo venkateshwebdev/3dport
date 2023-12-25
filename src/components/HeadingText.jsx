dc0155a31972138d97a647587a2f44e9a567c80d
venkateshwebdev <nikhilsirigineedi3@gmail.com>
2023-12-25 06:47:12 +0530

    sleep

--- src/components/HeadingText.jsx
+++ src/components/HeadingText.jsx
@@ -1,6 +1,6 @@
 const HeadingText = ({title}) => {
     return (
-        <div className="uppercase italic text-6xl max-md:text-4xl text-center mt-36 text-transparent font-bold hover:text-black z-50 transition-colors h-fit duration-1000" style={{WebkitTextStroke:"1px black"}}>{title}.</div>
+        <div className="uppercase italic text-6xl max-md:text-4xl text-center mt-36 text-transparent font-bold hover:text-black z-10 transition-colors h-fit duration-1000" style={{WebkitTextStroke:"1px black"}}>{title}.</div>
 
     );
 }
